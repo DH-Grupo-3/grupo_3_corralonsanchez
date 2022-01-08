afd79f0a64fdb51f3478f88bcdad50dfe11ee91c
TongasHub <ferreyragaston40@gmail.com>
2022-01-08 20:48:53 -0300

    Entrega sprint 2

--- routers/router.js
+++ routers/router.js
@@ -3,4 +3,12 @@ const controller = require('../controllers/baseController');
 
 router.route('/').get(controller.getIndex);
 
+router.route('/login').get(controller.login);
+
+router.route('/register').get(controller.register);
+
+router.route('/productdetail').get(controller.productDetail);
+
+router.route('/productcart').get(controller.productCart);
+
 module.exports = router;
