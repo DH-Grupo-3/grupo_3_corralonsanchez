9a7dbe76b025f0dd7ee95dfede0be7481c9e3055
TongasHub <ferreyragaston40@gmail.com>
2022-01-14 05:59:40 -0300

    config ejs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const app = require('./app.js');
-const PUERTO = 3000;
 
-app.listen(PUERTO, (req, res) => {
-  console.log(`Servidor escuchando puerto ${PUERTO}`);
+app.listen(app.get('port'), (req, res) => {
+  console.log(`Server listening on http://localhost:${app.get('port')}`);
 });
