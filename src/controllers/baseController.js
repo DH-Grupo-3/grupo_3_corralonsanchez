66efe797d23b8458362f15a9221688a08811743f
ivansanchez90 <sanchezoliva.ivan@gmail.com>
2022-02-08 08:23:41 -0300

    agrego vista productList con su ruta y controlador

--- src/controllers/baseController.js
+++ src/controllers/baseController.js
@@ -2,29 +2,33 @@ const path = require('path');
 const views = path.join(__dirname, '../views');
 
 exports.getIndex = (req, res, next) => {
-  res.render('index');
+	res.render('index');
 };
 
 exports.login = (req, res, next) => {
-  res.render('login');
+	res.render('login');
 };
 
 exports.register = (req, res, next) => {
-  res.render('register');
+	res.render('register');
 };
 
 exports.productDetail = (req, res, next) => {
-  res.render('productDetail');
+	res.render('productDetail');
 };
 
 exports.productCart = (req, res, next) => {
-  res.render('productCart');
+	res.render('productCart');
 };
 
 exports.productForm = (req, res, next) => {
-  res.render('productForm');
+	res.render('productForm');
 };
 
 exports.productEdit = (req, res, next) => {
-  res.render('productEdit');
+	res.render('productEdit');
+};
+
+exports.productList = (req, res, next) => {
+	res.render('productList');
 };
