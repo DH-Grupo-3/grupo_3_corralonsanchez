1f28be8795f2e7e769ce020a7afe80b7ac643a88
ivansanchez90 <sanchezoliva.ivan@gmail.com>
2022-02-08 22:32:00 -0300

    fix css vistas users

--- src/models/user.js
+++ src/models/user.js
@@ -5,37 +5,43 @@ const model = {
 	read: () => readFileSync(model.file),
 	convert: (data) => JSON.stringify(data, null, 2),
 	write: (data) => writeFileSync(model.file, model.convert(data)),
-	list: () => JSON.parse(model.read()).sort((a, b) => (a.id < b.id ? -1 : a.id > b.id ? 1 : 0)),
-	match: (propiedad, valor) => model.list().find((usuario) => usuario[propiedad] == valor),
+	list: () => JSON.parse(model.read()),
+	filter: (propiedad, valor) =>
+		model
+			.list()
+			.filter((producto) =>
+				typeof valor !== 'string' ? user[propiedad] == valor : user[propiedad].includes(valor),
+			),
+	match: (propiedad, valor) => model.list().find((user) => user[propiedad] == valor),
 	generate: (data) =>
 		Object({
 			id:
 				model.list().length > 0
 					? model
 							.list()
-
+							.sort((a, b) => (a.id < b.id ? -1 : a.id > b.id ? 1 : 0))
 							.pop().id + 1
 					: 1,
-			fullName: data.full_name,
+			full_name: data.full_name,
 			email: data.email,
 			password: data.password,
 			dni: Number(data.dni),
-			birth: data.date_of_birth,
+			date_of_birth: data.date_of_birth,
 		}),
 	create: (data) => {
-		let lista = model.list();
+		let lista = model.list().sort((a, b) => (a.id < b.id ? -1 : a.id > b.id ? 1 : 0));
 		lista.push(data);
 		model.write(lista);
 	},
 	update: (data) => {
-		let usuarios = model.list();
+		let usuarios = model.list().sort((a, b) => (a.id < b.id ? -1 : a.id > b.id ? 1 : 0));
 		usuarios = usuarios.map((usuario) => {
 			if (usuario.id == data.id) {
-				usuario.fullName = data.fullName;
+				usuario.full_name = data.full_name;
 				usuario.email = data.email;
 				usuario.password = data.password;
 				usuario.dni = data.dni;
-				usuario.birth = data.birth;
+				usuario.date_of_birth = data.date_of_birth;
 				return usuario;
 			}
 			return usuario;
@@ -43,7 +49,7 @@ const model = {
 		model.write(usuarios);
 	},
 	trash: (id) => {
-		let usuarios = model.list();
+		let usuarios = model.list().sort((a, b) => (a.id < b.id ? -1 : a.id > b.id ? 1 : 0));
 		model.write(usuarios.filter((usuario) => usuario.id != id));
 	},
 };
