4d9831040e7aefc74b2906f3d92b90152152f348
julipassotti <julietapassotti@gmail.com>
2022-02-17 20:58:44 -0300

    configuracion estilos para vistas de usuarios

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ app.use(cors());
 app.use(morgan('dev'));
 
 app.use(express.static(path.resolve(__dirname, '../public')));
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/', router);
 app.use('/users', require('./routers/users'));
