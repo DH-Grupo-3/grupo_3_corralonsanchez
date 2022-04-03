63a1ba8de8d65ec0fa7ec29446ff943edd8bcb28
TongasHub <ferreyragaston40@gmail.com>
2022-04-03 17:07:49 -0300

    validatemiddleware agregado y quitado de router/user.js

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,5 @@
 const { Router } = require('express');
 const router = Router();
-const { body } = require('express-validator');
 const {
 	show,
 	index,
@@ -13,33 +12,7 @@ const {
 } = require('../controllers/user');
 const guestMiddleware = require('../middlewares/guestMiddleware');
 const authMiddleware = require('../middlewares/authMiddleware');
-
-// Validaciones
-const validateForm = [
-	body('full_name').notEmpty().withMessage('Debes introducir tú nombre completo'),
-	body('address').notEmpty().withMessage('Debes introducir una dirección'),
-	body('cel').notEmpty().withMessage('Debes introducir un teléfono'),
-	body('email')
-		.notEmpty()
-		.withMessage('Debes introducir un email válido')
-		.bail()
-		.isEmail()
-		.withMessage('Debes introducir un email válido'),
-	body('password')
-		.notEmpty()
-		.withMessage('Debes introducir una contraseña válida')
-		.bail()
-		.isLength({ min: 8 })
-		.withMessage('La contraseña debe tener mínimo 8 caracteres'),
-	body('dni').notEmpty().withMessage('Debes introducir DNI'),
-	body('date_of_birth').notEmpty().withMessage('Debes introducir fecha de nacimiento'),
-	body('password2').custom((value, { req }) => {
-		if (value !== req.body.password) {
-			throw new Error('Las contraseñas no coinciden');
-		}
-		return true;
-	}),
-];
+const validateForm = require('../middlewares/validateFormMiddleware');
 
 router.get('/list', index);
 
