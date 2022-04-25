3a23f32716b26b6917cb96e7881bb7282eb6d2b5
IvanSanchez <sanchezoliva.ivan@gmail.com>
2022-04-25 17:51:37 -0300

    agregando fontawesome y submit al formulario

--- public/js/register.js
+++ public/js/register.js
@@ -130,8 +130,6 @@ formulario.addEventListener('submit', (e) => {
 		campos.password &&
 		campos.date_of_birth
 	) {
-		formulario.reset();
-
 		document
 			.getElementById('formulario__mensaje-exito')
 			.classList.add('formulario__mensaje-exito-activo');
@@ -144,6 +142,10 @@ formulario.addEventListener('submit', (e) => {
 
 		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
 			icono.classList.remove('formulario__grupo-correcto');
+
+			setTimeout(() => {
+				formulario.submit();
+			}, 3000);
 		});
 	} else {
 		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
